36180a29d0d1da96efbd732ff0040cda2bb72821
Ankit Raj <ankit.raj@hm.com>
2021-05-21 20:12:04 +0200

    more stuff added to come 1

--- es6-es8-js/arrow-stuff.js
+++ es6-es8-js/arrow-stuff.js
@@ -5,6 +5,9 @@ function add (a , b) {
 addme = add(4,5);
 console.log(addme);
 
+let addFn = (n,m) => n + m;
+console.log(addFn(4,4577));
+
 
 function printName(str1){
     console.log('Hello ' + str1 );
